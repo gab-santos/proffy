2290a8143998564877050d3a9dc676089f707806
Gabriel Santos <gab_santos@outlook.com>
2020-08-10 17:38:51 -0300

    finish favorites screen

--- mobile/src/screens/Favorites/index.tsx
+++ mobile/src/screens/Favorites/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 
+import ScreenHeader from "../../components/ScreenHeader";
+import TeacherItem from "../../components/TeacherItem";
 import styles from "./styles";
 
 const Favorites: React.FC = () => {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <ScreenHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={styles.teacherListContainer}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
+    </View>
+  );
 };
 
 export default Favorites;
--- mobile/src/screens/Favorites/styles.ts
+++ mobile/src/screens/Favorites/styles.ts
@@ -5,6 +5,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#f0f0f7",
   },
+
+  teacherList: {
+    marginTop: -60,
+  },
+
+  teacherListContainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+  },
 });
 
 export default styles;
